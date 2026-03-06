4269be1ee6c36f007c70ee975d4eb2d8bef17b39
n100 <n100@kun.com>
2026-03-07 00:12:11 +0800

    run: start app

--- src/main/java/com/jfeat/AmApplication.java
+++ src/main/java/com/jfeat/AmApplication.java
@@ -1,11 +1,17 @@
 package com.jfeat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({
+    "com.jfeat.org.services.persistence.dao",
+    "com.jfeat.org.services.domain.dao",
+    "com.jfeat.crud.plus.service.dao"
+})
 public class AmApplication {
     protected final static Logger logger = LoggerFactory.getLogger(AmApplication.class);
 
